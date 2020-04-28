29684c0e0b17071edb90a2e478301ae1c21a2594
baardl <bard.lind@gmail.com>
2020-04-28 19:57:20 +0200

    Login to ProptechOS/Idun successful

--- src/main/java/io/entraos/idun/client/ApiClient.java
+++ src/main/java/io/entraos/idun/client/ApiClient.java
@@ -1,13 +1,53 @@
 package io.entraos.idun.client;
 
-/**
- * Hello world!
- *
- */
-public class ApiClient
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import com.microsoft.aad.msal4j.ClientCredentialFactory;
+import com.microsoft.aad.msal4j.ClientCredentialParameters;
+import com.microsoft.aad.msal4j.ConfidentialClientApplication;
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import org.slf4j.Logger;
+
+import java.net.MalformedURLException;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class ApiClient {
+    private static final Logger log = getLogger(ApiClient.class);
+    public static final String SCOPE = "https://en.proptechos.com/api/.default";
+
+    private String accessToken;
+
+    public void login(String tenantId, String clientId, String clientSecret)
+            throws ExecutionException, InterruptedException, MalformedURLException {
+
+        ConfidentialClientApplication app =
+                ConfidentialClientApplication.builder(
+                        clientId, ClientCredentialFactory.createFromSecret(clientSecret))
+                        .authority("https://login.microsoftonline.com/" + tenantId + "/")
+                        .build();
+
+        ClientCredentialParameters clientCredentialParameters =
+                ClientCredentialParameters.builder(Collections.singleton(SCOPE)).build();
+
+        Future<IAuthenticationResult> future = app.acquireToken(clientCredentialParameters);
+
+        accessToken = future.get().accessToken();
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, MalformedURLException {
+        ApiClient apiClient = new ApiClient();
+        if (args.length == 3) {
+            apiClient.login(args[0], args[1], args[2]);
+            log.info("AccessToken is: {}", apiClient.getAccessToken());
+        } else {
+            log.error("Need 3 arguments tenantId, clientId and clientSecret");
+        }
     }
 }
