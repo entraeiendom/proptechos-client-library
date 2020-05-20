848e3921563aa999406f90fa8f0625091e0aaadf
baardl <bard.lind@gmail.com>
2020-05-20 13:36:43 +0200

    FetchBuildingComponentsCommand

--- src/main/java/io/entraos/idun/rec/buildingcomponents/FetchBuildingComponentsCommand.java
+++ src/main/java/io/entraos/idun/rec/buildingcomponents/FetchBuildingComponentsCommand.java
@@ -7,14 +7,25 @@ import java.net.URI;
 public class FetchBuildingComponentsCommand extends IdunGetCommand {
     private final String accessToken;
 
+    public static final int DEFAULT_PAGE_NUM = 0;
+    public static final int DEFAULT_FETCH_SIZE = 100;
+    private final int pageNum;
+    private final int fetchSize;
+
     public FetchBuildingComponentsCommand(URI baseURI, String accessToken) {
+        this(baseURI,accessToken, DEFAULT_PAGE_NUM, DEFAULT_FETCH_SIZE);
+    }
+
+    public FetchBuildingComponentsCommand(URI baseURI, String accessToken, int pageNum, int fetchSize) {
         super(baseURI,"BuildingComponents");
         this.accessToken = accessToken;
+        this.pageNum = pageNum;
+        this.fetchSize = fetchSize;
     }
 
     @Override
     protected URI buildUri() {
-        String fullUrl = getBaseUri().toString() + "json/buildingcomponent?page=0&size=50" ;
+        String fullUrl = getBaseUri().toString() + "json/buildingcomponent?page=" + pageNum +"&size=" + fetchSize ;
         URI fullUri = URI.create(fullUrl);
         return fullUri;
     }
