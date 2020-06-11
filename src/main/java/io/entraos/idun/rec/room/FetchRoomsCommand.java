7aa264ba503b246a057d6fe24c4d0ad8da2c2803
baardl <bard.lind@gmail.com>
2020-06-11 10:53:18 +0200

    FetchRooms with paging

--- src/main/java/io/entraos/idun/rec/room/FetchRoomsCommand.java
+++ src/main/java/io/entraos/idun/rec/room/FetchRoomsCommand.java
@@ -8,16 +8,26 @@ public class FetchRoomsCommand extends IdunGetCommand {
     private final String accessToken;
 
     private final String buildingUuid;
+    public static final int DEFAULT_PAGE_NUM = 0;
+    public static final int DEFAULT_FETCH_SIZE = 5000;
+    private final int pageNum;
+    private final int fetchSize;
 
     public FetchRoomsCommand(URI baseURI, String accessToken, String buildingUuid) {
-        super(baseURI, "Floor");
+        this(baseURI, accessToken,buildingUuid, DEFAULT_PAGE_NUM, DEFAULT_FETCH_SIZE);
+    }
+
+    public FetchRoomsCommand(URI baseURI, String accessToken, String buildingUuid, int pageNum, int fetchSize) {
+        super(baseURI, "Room");
         this.accessToken = accessToken;
+        this.pageNum = pageNum;
+        this.fetchSize = fetchSize;
         this.buildingUuid = buildingUuid;
     }
 
     @Override
     protected URI buildUri() {
-        String fullUrl = getBaseUri().toString() + "json/rooms?building_ids=" + buildingUuid ;
+        String fullUrl = getBaseUri().toString() + "json/room?building_ids=" + buildingUuid + "&page=" + pageNum + "&size=" + fetchSize ;
         URI fullUri = URI.create(fullUrl);
         return fullUri;
     }
