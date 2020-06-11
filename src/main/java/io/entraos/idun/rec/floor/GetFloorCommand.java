86834a5e4e8f0f2e0c538b63097001cecce83e85
baardl <bard.lind@gmail.com>
2020-06-11 14:20:10 +0200

    Get mapped floor.

--- src/main/java/io/entraos/idun/rec/floor/GetFloorCommand.java
+++ src/main/java/io/entraos/idun/rec/floor/GetFloorCommand.java
@@ -1,8 +1,11 @@
 package io.entraos.idun.rec.floor;
 
 import io.entraos.idun.commands.IdunGetCommand;
+import io.entraos.rec.domain.Floor;
+import io.entraos.rec.mappers.FloorJsonMapper;
 
 import java.net.URI;
+import java.net.http.HttpResponse;
 
 public class GetFloorCommand extends IdunGetCommand {
     private final String accessToken;
@@ -24,5 +27,16 @@ public class GetFloorCommand extends IdunGetCommand {
     @Override
     protected String buildAuthorization() {
         return "Bearer " + accessToken;
+    }
+
+    public Floor getFloor() {
+        String json = null;
+        HttpResponse<String> response = run();
+        json = response.body();
+        Floor floor = null;
+        if (json != null) {
+            floor = FloorJsonMapper.fromJson(json);
+        }
+        return floor;
     }
 }
